a0dafbf93dbb7e0f23eca64eab7c77bb16700624
nequals30 <nequals30@gmail.com>
2025-03-22 17:44:34 -0500

    cosmetic changes to encryption

--- src/encryption.jl
+++ src/encryption.jl
@@ -2,13 +2,18 @@ using Nettle, SHA, Base
 
 export encrypt_file, decrypt_file, ask_password, aes_encrypt, aes_decrypt
 
-# Added to the file before encrypting it. Used to make sure the decrpytion password is right.
-const HEADER_PASSWORD_CHECK = "PERSONALFINANCEJL"
-# Added to the file after encrypting it. Used to make sure decrpytion is being run on an encrypted file.
-const HEADER_I_AM_ENCRYPTED = "gflxXo8Gy54GjLch"
+# HEADER_B is added to the file before encrypting it. Used to make sure the decrpytion password is right.
+# HEADER_A is added to the file after encrypting it, to make sure decrpytion is being run on an encrypted file.
+# so an encrypted file looks like this: [HEADER_A, encrypted([HEADER_B, file_contents])]
+const HEADER_A = "gflxXo8Gy54GjLch"
+const HEADER_B = "PERSONALFINANCEJL"
 
+"""
+	encrypt_file(pathToFile::String)
+
+Creates an interactive dialogue to encrypt the file at the specified path.
+"""
 function encrypt_file(pathToFile::String)
-
 	# check if file exists
 	if !(isfile(pathToFile))
 		error("No such file: $(pathToFile)")
@@ -24,10 +29,16 @@ function encrypt_file(pathToFile::String)
 	# encryption
 	pwd = ask_password()
 	aes_encrypt(absPathToFile,pwd)
+	Base.shred!(pwd)
 end
 
-function decrypt_file(pathToFile::String)
 
+"""
+	decrypt_file(pathToFile::String)
+
+Creates an interactive dialogue to decrypt the file at the specified path.
+"""
+function decrypt_file(pathToFile::String)
 	# check if file exists
 	if !(isfile(pathToFile))
 		error("No such file: $(pathToFile)")
@@ -41,7 +52,9 @@ function decrypt_file(pathToFile::String)
 	pwd = Base.getpass("Enter password")
 	println()
 	aes_decrypt(absPathToFile,pwd)
+	Base.shred!(pwd)
 end
+
 
 function ask_password()
 	pwd = Base.getpass("Enter password")
@@ -58,8 +71,8 @@ function ask_password()
 	return pwd
 end
 
-function aes_encrypt(pathToFile::String,password::Base.SecretBuffer)
 
+function aes_encrypt(pathToFile::String,password::Base.SecretBuffer)
 	println()
 
 	# check if file exists
@@ -71,13 +84,12 @@ function aes_encrypt(pathToFile::String,password::Base.SecretBuffer)
 	plaintext = read(absPath)
 
 	# guardrail: make sure the file isn't already encrypted by this tool
-	if startswith(String(copy(plaintext)), HEADER_I_AM_ENCRYPTED)
+	if startswith(String(copy(plaintext)), HEADER_A)
         error("File appears to be already encrypted with this tool. Aborting.")
     end
-	plaintext_with_header = vcat(codeunits(HEADER_PASSWORD_CHECK), plaintext)
+	plaintext_with_header = vcat(codeunits(HEADER_B), plaintext)
 
 	key = sha256(read(password,String))
-	Base.shred!(password)
 
 	encrypter = Encryptor("AES256", key)
 	# `iv` ensures the same data is different when encrypted twice
@@ -85,7 +97,7 @@ function aes_encrypt(pathToFile::String,password::Base.SecretBuffer)
 	ciphertext = encrypt(encrypter, :CBC, iv, add_padding_PKCS5(plaintext_with_header,16))
 	
 	open(absPath, "w") do io
-		write(io, HEADER_I_AM_ENCRYPTED)
+		write(io, HEADER_A)
 		write(io, iv)
 		write(io, ciphertext)
 	end
@@ -94,6 +106,7 @@ function aes_encrypt(pathToFile::String,password::Base.SecretBuffer)
 	return
 
 end
+
 
 function aes_decrypt(pathToFile::String,password::Base.SecretBuffer)
 
@@ -108,26 +121,25 @@ function aes_decrypt(pathToFile::String,password::Base.SecretBuffer)
 	data = read(absPath)
 
 	# guardrail: check that plaintext header is there
-	expectedPlaintextHeader = String(data[1:length(HEADER_I_AM_ENCRYPTED)])
-	if expectedPlaintextHeader != HEADER_I_AM_ENCRYPTED
+	expectedPlaintextHeader = String(data[1:length(HEADER_A)])
+	if expectedPlaintextHeader != HEADER_A
 		error("File does not appear to have been encrypted by this program. Aborting.")
 	end
-	iv = data[(length(HEADER_I_AM_ENCRYPTED)+1):(length(HEADER_I_AM_ENCRYPTED)+16)]
-	ciphertext = data[(length(HEADER_I_AM_ENCRYPTED)+17):end]
+	iv = data[(length(HEADER_A)+1):(length(HEADER_A)+16)]
+	ciphertext = data[(length(HEADER_A)+17):end]
 
 	key = sha256(read(password,String))
-	Base.shred!(password)
 
 	decrypter = Decryptor("AES256",key)
 	padded_plaintext = decrypt(decrypter, :CBC, iv, ciphertext)
 
 	# guardrail: check that the password is correct
-	if !startswith(String(copy(padded_plaintext)), HEADER_PASSWORD_CHECK)
+	if !startswith(String(copy(padded_plaintext)), HEADER_B)
         error("Incorrect password. Aborting.")
     end
 
 	plaintext = trim_padding_PKCS5(padded_plaintext)
-    plaintext = plaintext[length(HEADER_PASSWORD_CHECK)+1:end]
+    plaintext = plaintext[length(HEADER_B)+1:end]
 
 	open(absPath,"w") do io
 		write(io,plaintext)
